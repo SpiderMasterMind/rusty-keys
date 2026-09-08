27483b0b76dae65b3880322dc478e4f1f13f208a
SpiderMasterMind <bstacey37@gmail.com>
2026-09-08 20:41:57 +0100

    Update to TCP single thread server

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
-use anyhow::{Result};
-use std::{fs::{self}, path::Path, time::{SystemTime, UNIX_EPOCH}, env};
-use std::collections::HashMap;
+use anyhow::Result;
 use serde::{Deserialize, Serialize};
-use serde_jsonlines::{json_lines, append_json_lines};
+use serde_jsonlines::{append_json_lines, json_lines};
+use std::collections::HashMap;
 use std::path::PathBuf;
+use std::{
+    env,
+    fs::{self},
+    path::Path,
+    time::{SystemTime, UNIX_EPOCH},
+};
 use thiserror::Error;
 
 struct Config {
@@ -11,7 +16,9 @@ struct Config {
 }
 
 fn config() -> Config {
-    Config { wal_path: default_wal_path() }
+    Config {
+        wal_path: default_wal_path(),
+    }
 }
 
 fn default_wal_path() -> PathBuf {
@@ -22,26 +29,25 @@ fn default_wal_path() -> PathBuf {
 
 #[derive(Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct Entry {
-  crc: String,
-  timestamp: String,
-  keysize: String,
-  valuesize: String,
-  key: String,
-  value: String
+    crc: String,
+    timestamp: String,
+    keysize: String,
+    valuesize: String,
+    key: String,
+    value: String,
 }
 
 impl Entry {
-  pub fn new(key: String, value: String) -> Self {
-    Self {
-      key: key,
-      value: value,
-      crc: String::from("CRC"),
-      keysize: String::from("1"),
-      valuesize: String::from("1"),
-      timestamp: String::from("123")
+    pub fn new(key: String, value: String) -> Self {
+        Self {
+            key: key,
+            value: value,
+            crc: String::from("CRC"),
+            keysize: String::from("1"),
+            valuesize: String::from("1"),
+            timestamp: String::from("123"),
+        }
     }
-
-  }
 }
 
 #[derive(Debug, Error)]
@@ -53,170 +59,196 @@ pub enum StoreError {
     WalReadFailed(#[from] anyhow::Error),
 }
 
-pub fn add<P: AsRef<Path>>(key: String, value: String, wal_path: Option<P>) -> Result<(String, String)> {
-  let now = SystemTime::now();
-  let duration = now.duration_since(UNIX_EPOCH).expect("time backwards");
+pub fn add<P: AsRef<Path>>(
+    key: String,
+    value: String,
+    wal_path: Option<P>,
+) -> Result<(String, String)> {
+    let now = SystemTime::now();
+    let duration = now.duration_since(UNIX_EPOCH).expect("time backwards");
 
-  let key_size = String::len(&key);
-  let value_size = String::len(&value);
+    let key_size = String::len(&key);
+    let value_size = String::len(&value);
 
-  let values = vec![
-    Entry {
-      crc: "CRC".to_string(),
-      timestamp: duration.as_millis().to_string(),
-      keysize: key_size.to_string(),
-      valuesize: value_size.to_string(),
-      key: key.clone(),
-      value: value.clone()
-    },
-  ];
+    let values = vec![Entry {
+        crc: "CRC".to_string(),
+        timestamp: duration.as_millis().to_string(),
+        keysize: key_size.to_string(),
+        valuesize: value_size.to_string(),
+        key: key.clone(),
+        value: value.clone(),
+    }];
 
-  let path = match wal_path {
-      Some(p) => p.as_ref().to_path_buf(),
-      None => config().wal_path,
-  };
+    let path = match wal_path {
+        Some(p) => p.as_ref().to_path_buf(),
+        None => config().wal_path,
+    };
 
-  append_json_lines(&path, &values)?;
-  Ok((key, value))
+    append_json_lines(&path, &values)?;
+    Ok((key, value))
 }
 
-pub fn replay_into_memory<P: AsRef<Path>>(wal_path: Option<P>) -> Result<Vec<Entry>> { 
-  let path = match wal_path {
-      Some(p) => p.as_ref().to_path_buf(),
-      None => config().wal_path,
-  };
-  let entries = json_lines(&path)?
-    .map(|r| r.map_err(anyhow::Error::msg))
-    .collect::<Result<Vec<Entry>>>()?;
-  Ok(entries)
+pub fn replay_into_memory<P: AsRef<Path>>(wal_path: Option<P>) -> Result<Vec<Entry>> {
+    let path = match wal_path {
+        Some(p) => p.as_ref().to_path_buf(),
+        None => config().wal_path,
+    };
+    let entries = json_lines(&path)?
+        .map(|r| r.map_err(anyhow::Error::msg))
+        .collect::<Result<Vec<Entry>>>()?;
+    Ok(entries)
 }
 
-pub fn read_from_memory<P: AsRef<Path>>(key: String, wal_path: Option<P>) -> Result<(String, String), StoreError> {
-  let entries = replay_into_memory(wal_path)?;
-  let mut result_hash = HashMap::new();
+pub fn read_from_memory<P: AsRef<Path>>(
+    key: String,
+    wal_path: Option<P>,
+) -> Result<(String, String), StoreError> {
+    let entries = replay_into_memory(wal_path)?;
+    let mut result_hash = HashMap::new();
 
-  for entry in entries {
-      result_hash.insert(entry.key, entry.value);
-  }
+    for entry in entries {
+        result_hash.insert(entry.key, entry.value);
+    }
 
-  let value = result_hash
-    .get(&key)
-    .cloned()
-    .ok_or_else(|| StoreError::NotFound(key.clone()))?;
+    let value = result_hash
+        .get(&key)
+        .cloned()
+        .ok_or_else(|| StoreError::NotFound(key.clone()))?;
 
-  Ok((key, value))
+    Ok((key, value))
 }
 
-pub fn all <P: AsRef<Path>>(wal_path: Option<P>) -> Result<String> {
-  if let Some(path) = wal_path {
-    Ok(fs::read_to_string(path)?)
-  } else {
-    Ok(fs::read_to_string(Path::new(&config().wal_path))?)
-  }
+pub fn all<P: AsRef<Path>>(wal_path: Option<P>) -> Result<String> {
+    if let Some(path) = wal_path {
+        Ok(fs::read_to_string(path)?)
+    } else {
+        Ok(fs::read_to_string(Path::new(&config().wal_path))?)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
-  use tempfile::{tempdir};
-  use std::io::Write;
-  use std::fs::File;
-  use std::time::{SystemTime, UNIX_EPOCH};
-  use assert_matches_regex::assert_matches_regex;
+    use super::*;
+    use assert_matches_regex::assert_matches_regex;
+    use std::fs::File;
+    use std::io::Write;
+    use std::time::{SystemTime, UNIX_EPOCH};
+    use tempfile::tempdir;
 
-  #[test]
-  fn all_returns_kvs_in_wal_log() -> () {
-    let dir = tempdir().unwrap();
-    let file_path = dir.path().join("temp_wal.log");
-    let mut file = File::create(&file_path).unwrap();
+    #[test]
+    fn all_returns_kvs_in_wal_log() -> () {
+        let dir = tempdir().unwrap();
+        let file_path = dir.path().join("temp_wal.log");
+        let mut file = File::create(&file_path).unwrap();
 
-    let now = SystemTime::now();
-    let duration = now.duration_since(UNIX_EPOCH).expect("time backwards");
+        let now = SystemTime::now();
+        let duration = now.duration_since(UNIX_EPOCH).expect("time backwards");
 
-    let key = "a";
-    let key_size = size_of_val(key);
-    let value_string = "Hello World!";
-    let value_size = size_of_val(value_string);
-    
+        let key = "a";
+        let key_size = size_of_val(key);
+        let value_string = "Hello World!";
+        let value_size = size_of_val(value_string);
 
-    let contents = format!("{},{:?},{},{},{},{}", "CRC".to_string(), duration.as_millis(), key_size.to_string(), value_size.to_string(), key, value_string);
-    let contents2 = format!("{},{:?},{},{},{},{}", "CRC".to_string(), duration.as_millis(), key_size.to_string(), value_size.to_string(), key, value_string);
-    writeln!(file, "{}\n{}", contents, contents2).unwrap();
+        let contents = format!(
+            "{},{:?},{},{},{},{}",
+            "CRC".to_string(),
+            duration.as_millis(),
+            key_size.to_string(),
+            value_size.to_string(),
+            key,
+            value_string
+        );
+        let contents2 = format!(
+            "{},{:?},{},{},{},{}",
+            "CRC".to_string(),
+            duration.as_millis(),
+            key_size.to_string(),
+            value_size.to_string(),
+            key,
+            value_string
+        );
+        writeln!(file, "{}\n{}", contents, contents2).unwrap();
 
-    assert_matches_regex!(all(Some(file_path)).unwrap(), r"CRC,\d+,1,12,a,Hello World!")
-  }
+        assert_matches_regex!(
+            all(Some(file_path)).unwrap(),
+            r"CRC,\d+,1,12,a,Hello World!"
+        )
+    }
 
-  #[test]
-  fn add_adds_and_returns_kv_added() -> (){
-    let tmp_dir = tempdir().unwrap();
-    let file_path = tmp_dir.path().join("my-temporary-note.txt");
-    File::create(&file_path).unwrap();
+    #[test]
+    fn add_adds_and_returns_kv_added() -> () {
+        let tmp_dir = tempdir().unwrap();
+        let file_path = tmp_dir.path().join("my-temporary-note.txt");
+        File::create(&file_path).unwrap();
 
-    let add_result = add("A".to_string(), "Egg".to_string(), Some(&file_path)).unwrap();
+        let add_result = add("A".to_string(), "Egg".to_string(), Some(&file_path)).unwrap();
 
-    assert_eq!(add_result, (String::from("A"), String::from("Egg")))
-  }
+        assert_eq!(add_result, (String::from("A"), String::from("Egg")))
+    }
 
-  #[test]
-  fn read_from_memory_returns_the_keyed_value_when_it_exists() -> (){
-    let tmp_dir = tempdir().unwrap();
-    let file_path = tmp_dir.path().join("my-temporary-note.txt");
-    File::create(&file_path).unwrap();
+    #[test]
+    fn read_from_memory_returns_the_keyed_value_when_it_exists() -> () {
+        let tmp_dir = tempdir().unwrap();
+        let file_path = tmp_dir.path().join("my-temporary-note.txt");
+        File::create(&file_path).unwrap();
 
-    add("A".into(), "Egg".into(), Some(&file_path)).unwrap();
-    add("B".into(), "Man".into(), Some(&file_path)).unwrap();
+        add("A".into(), "Egg".into(), Some(&file_path)).unwrap();
+        add("B".into(), "Man".into(), Some(&file_path)).unwrap();
 
-    let result = read_from_memory("B".to_string(),Some(&file_path));
+        let result = read_from_memory("B".to_string(), Some(&file_path));
 
-    assert_eq!(result.unwrap(), (String::from("B"), String::from("Man")), "incorrect value")
-  }
+        assert_eq!(
+            result.unwrap(),
+            (String::from("B"), String::from("Man")),
+            "incorrect value"
+        )
+    }
 
-  #[test]
-  fn read_from_memory_returns_an_err_when_it_doesnt_exist() -> (){
-    let tmp_dir = tempdir().unwrap();
-    let file_path = tmp_dir.path().join("my-temporary-note.txt");
-    File::create(&file_path).unwrap();
+    #[test]
+    fn read_from_memory_returns_an_err_when_it_doesnt_exist() -> () {
+        let tmp_dir = tempdir().unwrap();
+        let file_path = tmp_dir.path().join("my-temporary-note.txt");
+        File::create(&file_path).unwrap();
 
-    add("A".into(), "Egg".into(), Some(&file_path)).unwrap();
+        add("A".into(), "Egg".into(), Some(&file_path)).unwrap();
 
-    let result = read_from_memory("B".to_string(),Some(&file_path));
+        let result = read_from_memory("B".to_string(), Some(&file_path));
 
-    // assert_eq!(result, String::from("Not found"), "incorrect value")
-    assert!(matches!(result, Err(StoreError::NotFound(_))));
-  }
+        assert!(matches!(result, Err(StoreError::NotFound(_))));
+    }
 
-  #[test]
-  fn replay_into_memory_collects_jsonl_entries_into_entry_vec() -> (){
-    let tmp_dir = tempdir().unwrap();
-    let file_path = tmp_dir.path().join("my-temporary-note.txt");
-    File::create(&file_path).unwrap();
+    #[test]
+    fn replay_into_memory_collects_jsonl_entries_into_entry_vec() -> () {
+        let tmp_dir = tempdir().unwrap();
+        let file_path = tmp_dir.path().join("my-temporary-note.txt");
+        File::create(&file_path).unwrap();
 
-    add("A".into(), "Egg".into(), Some(&file_path)).unwrap();
-    add("B".into(), "Man".into(), Some(&file_path)).unwrap();
-    add("C".into(), "Walrus".into(), Some(&file_path)).unwrap();
+        add("A".into(), "Egg".into(), Some(&file_path)).unwrap();
+        add("B".into(), "Man".into(), Some(&file_path)).unwrap();
+        add("C".into(), "Walrus".into(), Some(&file_path)).unwrap();
 
-    let result = replay_into_memory(Some(&file_path)).unwrap();
+        let result = replay_into_memory(Some(&file_path)).unwrap();
 
-    let _expected: Vec<Entry> = vec![("A", "Egg"), ("B", "Man")]
-      .iter()
-      .cloned()
-      .map(|(k, v)| Entry::new(k.to_string(), v.to_string()))
-      .collect();
+        let _expected: Vec<Entry> = vec![("A", "Egg"), ("B", "Man")]
+            .iter()
+            .cloned()
+            .map(|(k, v)| Entry::new(k.to_string(), v.to_string()))
+            .collect();
 
-    assert_eq!(result.len(), 3);
+        assert_eq!(result.len(), 3);
 
-    let Entry { key, value, .. } = &result[2];
-    assert_eq!(key, "C");
-    assert_eq!(value, "Walrus");
+        let Entry { key, value, .. } = &result[2];
+        assert_eq!(key, "C");
+        assert_eq!(value, "Walrus");
 
-    let Entry { key, value, .. } = &result[1];
-    assert_eq!(key, "B");
-    assert_eq!(value, "Man");
+        let Entry { key, value, .. } = &result[1];
+        assert_eq!(key, "B");
+        assert_eq!(value, "Man");
 
-    let Entry { key, value, .. } = &result[0];
-    assert_eq!(key, "A");
-    assert_eq!(value, "Egg");
-  }
+        let Entry { key, value, .. } = &result[0];
+        assert_eq!(key, "A");
+        assert_eq!(value, "Egg");
+    }
 }
 
 // crc,32bittimestamp,keysize,valuesize,key,value
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use clap::{Subcommand, Parser};
 use rusty_keys::{add, all, read_from_memory};
-use std::path::PathBuf;
+use clap::{Parser, Subcommand};
+use smol::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use smol::{Async, io};
+use std::net::{TcpListener, TcpStream};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -11,38 +13,94 @@ struct Args {
 
 #[derive(Subcommand, Debug)]
 enum Command {
-    Add { text: Vec<String> },
+    Add { inputs: Vec<String> },
     All,
-    Get {key: String }
+    Get { input: Vec<String> },
 }
 
-fn main()-> Result<(), anyhow::Error> {
-  let args = Args::parse();
+const WAL_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/wal.log");
 
-  match args.command {
-    Command::Add { text } => {
-      let text_collection: Vec<String> = text.into_iter().clone().collect();
+async fn handle(stream: Async<TcpStream>) -> io::Result<()> {
+    let mut reader = BufReader::new(&stream);
+    let mut line = String::new();
 
-      if text_collection.len() == 1 {
-        print!("no")
-      } else if text_collection.len() > 2 {
-        print!("no")
-      } else {
-        let result = add(text_collection[0].clone(), text_collection[1].clone(), None::<PathBuf>);
-        println!("{:?}", result)
-      }
-    },
-    Command::All => {
-      let result = all(None::<PathBuf>)?;
-      println!("{:?},", result)
-    },
-    Command::Get { key} => {
-      let result = read_from_memory(key, None::<PathBuf>);
-      println!("{:?}", result)
+    let mut writer = &stream;
+    let mut response: String;
+
+    loop {
+        line.clear();
+        let bytes_read = reader.read_line(&mut line).await?;
+
+        if bytes_read == 0 {
+            break;
+        }
+
+        let command = &line.trim_end();
+        let parts = command.split_whitespace();
+
+        let args = std::iter::once("rusty-keys").chain(parts);
+
+        match Args::try_parse_from(args) {
+            Ok(parsed) => match parsed.command {
+                Command::Add { inputs } => {
+                    if inputs.len() < 2 {
+                        response = format!("ADD called with not enough arguments!");
+                    } else {
+                        let (first, rest) = inputs.split_first().unwrap();
+                        let result = add(first.to_string(), rest.join(" "), Some(WAL_PATH));
+
+                        response = match result {
+                          Ok(v) => format!("ADD {:?}", v),
+                          Err(e) => format!("Error: {:?}", e)
+                        }
+                    }
+                }
+                Command::All => {
+                    let contents = all(Some(WAL_PATH));
+                    response = contents.unwrap();
+                }
+                Command::Get { input } => {
+                    if input.len() > 1 {
+                        response = format!("GET called with too many arguments!");
+                    } else {
+                        let key = input.clone().remove(0);
+                        let result = read_from_memory(key, Some(WAL_PATH));
+
+                        response = match result {
+                          Ok(v) => v.1,
+                          Err(e) => format!("Error for GET: {:?}", e)
+                        }
+                    }
+                }
+            },
+            Err(e) => {
+                writer
+                    .write_all(format!("Error: {}\n", e).as_bytes())
+                    .await?;
+                continue;
+            }
+        }
+
+        writer
+            .write_all(format!("{}\n", response).as_bytes())
+            .await?;
     }
-  }
-  Ok(())
+
+    Ok(())
 }
 
+fn main() -> io::Result<()> {
+    smol::block_on(async {
+        let listener = Async::<TcpListener>::bind(([127, 0, 0, 1], 7000))?;
+        println!("Listening on {}", listener.get_ref().local_addr()?);
+
+        loop {
+            let (stream, peer_addr) = listener.accept().await?;
+            println!("Accepted client: {}", peer_addr);
+
+            smol::spawn(handle(stream)).detach();
+        }
+    })
+}
 
 // hint files used for merge, compaction, speedy access
